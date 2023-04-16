ce0f388aeb0bef707b4b4fcfe625f9004fac7c5f
Viktor Biruk <vikttur.job@gmail.com>
2023-04-17 00:37:50 +0300

    Performs task 1 - creating an image gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,3 @@
-// Add imports above this line
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
@@ -35,8 +34,5 @@ const instanceLightBox = new SimpleLightbox('a', {
 	captionsData: 'alt',
 	captionsDelay: 250,
 });
-
-
-// Change code below this line
 
 console.log(galleryItems);
