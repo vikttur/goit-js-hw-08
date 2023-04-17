ac390d8427d42fbf8f56c8f7efbe62baa73b68d9
Viktor Biruk <vikttur.job@gmail.com>
2023-04-17 11:37:25 +0300

    Performs task 1 - the current time of the video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import vimPlayer from '@vimeo/player';
+import lodashTrottle from 'lodash.throttle';
 
+const TIME_PLAYER= 'videoplayer-current-time';
+const iframeRef = document.querySelector('iframe');
+const player = new vimPlayer(iframeRef);
+
+player.on('timeupdate', lodashTrottle(onPlay,1000));
+
+function onPlay(event) {
+	localStorage.setItem(TIME_PLAYER, event.seconds);
+};
+
+setCurrentTime();
+
+function setCurrentTime() {
+	if (!localStorage.getItem(TIME_PLAYER)) {
+		return;
+	};
+
+	player.setCurrentTime(localStorage.getItem(TIME_PLAYER));
+};
+
+
+player.on('play', function() {
+	console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+	console.log('title:', title);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import '../css/common.css';
+import '../css/03-feedback.css';
 
+
+const formRef = document.querySelector('js-feedback-form');
+const textareaRef = document.querySelector('js-feedback-form textarea');
+console.log(555);
+
+
+
+
+
+
+
+formRef.addEventListener('submit', onFormSubmit);
+textareaRef.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+
+	event.currentTarget.reset();
+	localStorage.removeItem('feedback-form-state');
+};
+
+function onTextareaInput(event) {
+	const message = event.currentTarget.value;
+	
+	localStorage.setItem('feedback-form-state', message);
+};
+
+function populateTextarea() {
+	const saveMessage = localStorage.getItem('feedback-form-state');
+
+	if (saveMessage) {
+		textareaRef.value = saveMessage;
+	};
+};
